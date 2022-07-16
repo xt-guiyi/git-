8e16079c73f4874afd65af6ab4550236a18fb18a
xt_guiyi <1661219752@qq.com>
2022-07-16 22:23:40 +0800

    Revert "Merge pull request #1 from xt-guiyi/feature2"

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 //master的js
 prison()
-console.log('哈')
 function prison() {
   console.log(new Date().now())
 }
-console.log('main')
-console.log('冲突')
-console.log('feature1')
-console.log('feature2')
-var s = 1;
-var dd = 1;
-var sd =1
-var sdd =1
